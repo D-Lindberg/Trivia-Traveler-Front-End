8cbc091a27bd9bae41e98163b8f2d962b676121c
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-08-15 06:18:26 -0500

    replaced random arrangement with sorted arrangment and removed all but 2 of the console logs

--- src/pages/TriviaPage.js
+++ src/pages/TriviaPage.js
@@ -11,18 +11,14 @@ export default class TriviaPage extends Component {
   }
 
   fetchTriviaQuestion = async () => {
-    console.log("about to fetch");
     let response = await fetch("https://opentdb.com/api.php?amount=1&encode=base64")
-    console.log(response)
     let json = await response.json()
     console.log(json);
     return json
   }
 
   prepareQuestion = async () => {
-    console.log("about to send to the fetcher");
     let encodedTrivia = await this.fetchTriviaQuestion()
-    console.log(encodedTrivia);
     let category = atob(encodedTrivia.results[0].category)
     let type = atob(encodedTrivia.results[0].type)
     let difficulty = atob(encodedTrivia.results[0].difficulty)
@@ -30,40 +26,44 @@ export default class TriviaPage extends Component {
     let correct_answer = atob(encodedTrivia.results[0].correct_answer)
     console.log(correct_answer);
     let incorrect_answers = encodedTrivia.results[0].incorrect_answers
-    console.log(incorrect_answers)
     let wrong_answers = incorrect_answers.map(answer => atob(answer))
-    console.log(wrong_answers)
     let multiple = type === "multiple"
     this.setState({ Trivia: { category: category, difficulty: difficulty, question: question, correct_answer: correct_answer, incorrect_answers: wrong_answers }, multiple: multiple })
   }
 
   componentDidMount() {
-    console.log("component mounted")
     this.prepareQuestion()
   }
 
-  //if not true or false. randomize answers.
+  //sorted list will work for 99% of the questions
+  // perhaps check for "all of the above" or similar as it isn't consistent
+  //and then move that to the last entry
   arrange_answers_for_display() {
     if (this.state.multiple === false) {
       return ['True', 'False']
+    } else if (this.state.multiple === true) {
+      let answer_list = this.state.Trivia.incorrect_answers.slice()
+      answer_list.push(this.state.Trivia.correct_answer)
+      return answer_list.sort()
     } else {
-      let answer_list = [...this.state.incorrect_answers]
-      answer_list.push(this.state.correct_answer)
-      let currentIndex, temp, randomIndex
-      currentIndex = answer_list.length
-      while (currentIndex !== 0){
-        randomIndex = Math.floor(Math.random() * currentIndex)
-        currentIndex -= 1
-        temp = answer_list[currentIndex]
-        answer_list[currentIndex] = answer_list[randomIndex]
-        answer_list[randomIndex] = temp
-      }
-      return answer_list
+      return ['','','','']
     }
   }
 
+  prepareAnswersForDisplay () {
+    let answerSequence = this.arrange_answers_for_display()
+    let toBeDisplayed = answerSequence.map((answer, index) => {
+      return (
+        <FormGroup check key={index}>
+          <Label check>
+            <Input type="radio" name="answer" />
+              {' '}{answer}
+          </Label>
+        </FormGroup>)
+    })
+    return toBeDisplayed
+  }
 
-  //display the question
   //onsubmit verify the answer
   //update user currency 
   //display options to get another question
@@ -74,40 +74,11 @@ export default class TriviaPage extends Component {
       return (
         <div style={{display: 'block'}}>
         <Form tag="fieldset" style={{display: 'inline-block', margin:"auto"}}>
-          <legend>Category: {this.state.Trivia.category}</legend>
+          <legend>{this.state.Trivia.category}</legend>
           <p>Difficulty: {this.state.Trivia.difficulty}</p>
           <p>{this.state.Trivia.question}</p>
           <Col sm={10} style={{textAlign:"left"}}>
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="answer1" />{' '}
-              {this.state.Trivia.correct_answer}
-            </Label>
-          </FormGroup>
-
-          <FormGroup check>
-            <Label check>
-              <Input type="radio" name="answer1" />{' '}
-              {this.state.Trivia.incorrect_answers[0]}
-            </Label>
-          </FormGroup>
-
-          {this.state.multiple &&
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="answer1" />{' '}
-                {this.state.Trivia.incorrect_answers[1]}
-              </Label>
-            </FormGroup>
-          }
-          {this.state.multiple &&
-            <FormGroup check>
-              <Label check>
-                <Input type="radio" name="answer1" />{' '}
-                {this.state.Trivia.incorrect_answers[2]}
-              </Label>
-            </FormGroup>
-          }
+          {this.prepareAnswersForDisplay()}
           </Col>
           <Button>Submit</Button>
         </Form>
